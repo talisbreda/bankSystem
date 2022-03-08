a9748f0b82379558a98cfe2d0f83c38ffbfe93e9
talisbreda <talis.breda@outlook.com>
2022-03-07 21:14:45 -0300

    Added Input Mismatch Exception treatment

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,7 +8,6 @@ public class Account {
     protected double balance;
 
     Random random = new Random();
-    Scanner scan = new Scanner(System.in);
 
     public Account() {
         this.agency = 1;
@@ -20,49 +20,82 @@ public class Account {
         this.number = number;
     }
 
+    boolean stop = false;
+
     public void withdraw() {
-        System.out.println("Insert the value of the withdrawal");
-        double value = scan.nextDouble();
-        if (value > this.balance) {
-            System.out.println("Insufficient balance.");
-        } else {
-            this.balance -= value;
-            System.out.printf("New balance on account %d: US$%.2f %n", this.number, this.balance);
-        }
+        stop = false;
+        do {
+            Scanner scan = new Scanner(System.in);
+            try {
+                System.out.println("Insert the value of the withdrawal");
+                double value = scan.nextDouble();
+                if (value > this.balance) {
+                    System.out.println("Insufficient balance.");
+                } else {
+                    this.balance -= value;
+                    stop = true;
+                    System.out.printf("New balance on account %d: US$%.2f %n", this.number, this.balance);
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Please insert a valid number");
+            }
+        } while (!stop);
     }
+
+
     public void withdraw(double value) {
         this.balance -= value;
     }
 
     public void deposit() {
-        System.out.println("Insert the value of the deposit");
-        double value = scan.nextDouble();
-        this.balance += value;
-        System.out.printf("New balance on this %d: US$%.2f %n", this.number, this.balance);
+        stop = false;
+        do {
+            Scanner scan = new Scanner(System.in);
+            try {
+                System.out.println("Insert the value of the deposit");
+                double value = scan.nextDouble();
+                this.balance += value;
+                stop = true;
+                System.out.printf("New balance on this %d: US$%.2f %n", this.number, this.balance);
+            } catch (InputMismatchException e) {
+                System.out.println("Please insert a valid number");
+            }
+        } while (!stop);
     }
+
     public void deposit(double value) {
         this.balance += value;
     }
 
     public void transfer() {
-        System.out.println("Insert the agency and number of the destination account");
-        int destinationAccountAgency = scan.nextInt();
-        int destinationAccountNumber = scan.nextInt();
+        stop = false;
+        do {
+            Scanner scan = new Scanner(System.in);
+            try {
+                System.out.println("Insert the agency and number of the destination account");
+                int destinationAccountAgency = scan.nextInt();
+                int destinationAccountNumber = scan.nextInt();
 
-        Account account1 = new Account(destinationAccountAgency, destinationAccountNumber);
+                Account account1 = new Account(destinationAccountAgency, destinationAccountNumber);
 
-        System.out.println("Insert the value of the transfer");
-        double value = scan.nextDouble();
+                System.out.println("Insert the value of the transfer");
+                double value = scan.nextDouble();
 
-        if (value > this.balance) {
-            System.out.println("Insufficient balance.");
-        } else {
-            this.withdraw(value);
-            account1.deposit(value);
-            System.out.println("Transfer successful.");
-            System.out.printf("New balance on account %s: US$%.2f %n", this.number, this.balance);
-            System.out.printf("New balance on account %s: US$%.2f %n", account1.number, account1.balance);
-        }
+                if (value > this.balance) {
+                    System.out.println("Insufficient balance.");
+                } else {
+                    this.withdraw(value);
+                    account1.deposit(value);
+                    stop = true;
+                    System.out.println("Transfer successful.");
+                    System.out.printf("New balance on account %s: US$%.2f %n", this.number, this.balance);
+                    System.out.printf("New balance on account %s: US$%.2f %n", account1.number, account1.balance);
+                }
+            } catch(InputMismatchException e) {
+                System.out.println("Please insert a valid number");
+            }
+        } while (!stop);
     }
 
 }
--- src/Bank.java
+++ src/Bank.java
@@ -1,31 +1,42 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Bank {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
         Account account = new Account();
-        int input = 0;
+        boolean stop = false;
+        int input;
 
-        while (input != 4) {
-            System.out.println("1 to Withdraw, 2 to deposit, 3 to transfer, 4 to stop");
-            input = scan.nextInt();
+        do {
+            try {
+                Scanner scan = new Scanner(System.in);
+                System.out.println("1 to Withdraw, 2 to deposit, 3 to transfer, 4 to stop");
 
-            switch (input) {
-                case 1:
-                    account.withdraw();
-                    break;
-                case 2:
-                    account.deposit();
-                    break;
-                case 3:
-                    account.transfer();
-                    break;
-                default:
-                    System.out.println("Please enter a valid number");
-                    break;
+                input = scan.nextInt();
+
+                switch (input) {
+                    case 1:
+                        account.withdraw();
+                        break;
+                    case 2:
+                        account.deposit();
+                        break;
+                    case 3:
+                        account.transfer();
+                        break;
+                    case 4:
+                        stop = true;
+                        break;
+                    default:
+                        System.out.println("Please enter a valid number");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please insert a valid number");
             }
-        }
+        } while (!stop);
 
     }
+
 }
+
